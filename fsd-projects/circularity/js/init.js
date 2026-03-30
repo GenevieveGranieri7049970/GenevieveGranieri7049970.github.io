4207a1a993085e932995b937f7dbc06425e5909c
GenevieveGranieri7049970 <grg7049970@lpssonline.com>
2026-03-30 13:32:43 +0000

    todo 7 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -34,15 +34,13 @@ var circles = []
 
 
         // TODO 3 : Call the drawCircle() function
-drawCircle()
-drawCircle()
-drawCircle()
-drawCircle()
 
 
 
         // TODO 7 : Use a loop to create multiple circles
-
+for(var loopsCompleted = 0; loopsCompleted < 175; loopsCompleted++){
+    drawCircle();
+}
 
 
 
